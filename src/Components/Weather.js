e7bac5a4695c4ae6783304bbe8990d028a2264ea
kris <krisekteles@gmail.com>
2025-07-31 20:48:48 +0300

    function to make the API call, an async function

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -7,6 +7,19 @@ import humidity_icon from "../assets/temperature-half-solid-full.svg";
 import wind_icon from "../assets/wind-solid-full.svg";
 
 const Weather = () => {
+    const search= async (city)=>{
+       try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+          import.meta.env.REACT_APP_API_KEY}`;
+          const response=await fetch(url);
+          const data = await response.json();
+
+          console.log(data);
+        
+       } catch (error) {
+        
+       }
+    }
   return (
     <div className="weather">
       <div className="search-bar">
@@ -14,9 +27,28 @@ const Weather = () => {
         <img src={search_icon} alt="search" />
       </div>
       <img src={cloud_icon} alt="" className="weather-icon" />
-      <p className='temperature'>
-        16 <sup>o</sup> celcius
+      <p className="temperature">
+        16 <sup>o</sup> c
       </p>
+      <p className="location">London</p>
+      <div className="weather-data">
+        <div className="col">
+         
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.6 km/h</p>
+            <span>wind speed</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
